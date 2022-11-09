530f640c703589813a4973ddae0de38956243530
Marvel <marvelphannn@gmail.com>
2022-11-09 14:55:22 +0700

    setup environment

---
+++ rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/controller/HomeController.java
@@ -0,0 +1,12 @@
+package apap.tugasAkhir.rumahSehat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    private String home(){
+        return "home/home";
+    }
+}
