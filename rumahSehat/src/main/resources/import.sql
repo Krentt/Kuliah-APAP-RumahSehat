08d548c6e5f15b7148b4860d52d3aefead673a5c
Marvel <marvelphannn@gmail.com>
2022-11-12 10:00:14 +0700

    feat: add import.sql

---
+++ rumahSehat/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into role_model values (1, "Admin");
+insert into role_model values (2, "Pasien");
+insert into role_model values (3, "Dokter");
+insert into role_model values (4, "Apoteker");
