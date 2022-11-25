64bedc739352c600a03ee8afacea425ebc6d0f57
Cisco Wicaksana <cisco@wicaksana.org>
2022-11-25 22:05:44 +0700

    cisco: 1) create basis for appointment. 2) change TagihanModel with accordance to AppointmentModel

--- rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/model/TagihanModel.java
+++ rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/model/TagihanModel.java
@@ -2,22 +2,23 @@ package apap.tugasAkhir.rumahSehat.model;
 
 import apap.tugasAkhir.rumahSehat.model.JumlahModel;
 import apap.tugasAkhir.rumahSehat.model.ResepModel;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class TagihanModel {
+@Entity
+public class TagihanModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //TODO: Generate according to soal: BILL-x (BILL-1, BILL-2, ...)
     private String id;
 
     @NotNull
@@ -40,20 +41,26 @@ public class TagihanModel {
     @Column(name = "total")
     private Integer total;
 
-    @NotNull
-    @Size(max = 50)
-    @Column(name = "kodeAppointment")
-    private String kode_appointment;
+//    @NotNull
+//    @Size(max = 50)
+//    @Column(name = "kodeAppointment")
+//    private String kode_appointment;
+
+    // Relasi dengan appointment
+    @JsonManagedReference
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "kode", referencedColumnName = "kodeAppointment") // TODO: ga yakin bener
+    private AppointmentModel appointmentModel;
 
     //For Dummy Testing, TODO: REMOVE when apppointment is done
-    private ResepModel resep;
+//    private ResepModel resep;
     private int totalResep;
     private int tarifDokter;
 
     public void calculateTotal(){
         int totalCurr = 0;
 
-        for (JumlahModel j: this.resep.getListJumlahModel()
+        for (JumlahModel j: this.appointmentModel.getResepModel().getListJumlahModel()
              ) {
             totalCurr += j.getObat().getHarga() * j.getKuantitas();
         }
---
+++ rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/service/AppointmentService.java
@@ -0,0 +1,9 @@
+package apap.tugasAkhir.rumahSehat.service;
+
+import apap.tugasAkhir.rumahSehat.model.AppointmentModel;
+
+import java.util.List;
+
+public interface AppointmentService {
+    List<AppointmentModel> getListAppointment();
+}
