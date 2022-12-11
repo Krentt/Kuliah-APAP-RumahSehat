776d3530fa6e14f144fb454e92ee761b8b573d6d
Shabiqa Amani <shabiqa.amani@ui.ac.id>
2022-12-11 20:52:49 +0700

    Shabiqa: Feature 11

--- rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/repository/ApotekerDb.java
+++ rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/repository/ApotekerDb.java
@@ -1,9 +1,11 @@
 package apap.tugasAkhir.rumahSehat.repository;
 
 import apap.tugasAkhir.rumahSehat.model.ApotekerModel;
+import apap.tugasAkhir.rumahSehat.model.ResepModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface ApotekerDb extends JpaRepository<ApotekerModel, Long> {
+    Optional<ApotekerModel> findByUsername(String username);
 }
