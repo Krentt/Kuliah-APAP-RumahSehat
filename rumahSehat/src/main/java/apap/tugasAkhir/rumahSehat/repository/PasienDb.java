960965e08c738fbeffe87bc9200816739bce6f78
Cisco Wicaksana <cisco@wicaksana.org>
2022-12-12 13:56:28 +0700

    cisco/fase3: last sonarqube smellies before package renaming

--- rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/repository/PasienDb.java
+++ rumahSehat/src/main/java/apap/tugasAkhir/rumahSehat/repository/PasienDb.java
@@ -4,10 +4,8 @@ import apap.tugasAkhir.rumahSehat.model.PasienModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.io.Serializable;
-
 @Repository
-public interface PasienDb extends JpaRepository<PasienModel, Long>, Serializable {
+public interface PasienDb extends JpaRepository<PasienModel, Long> {
     PasienModel findById(String id);
     PasienModel findByUsername(String username);
 }
